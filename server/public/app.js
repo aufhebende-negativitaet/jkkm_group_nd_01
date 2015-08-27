685842ddf767eece7f7dab34037b22df33fa38e6
James McElwain <jcm@MacBook-Pro.local>
2015-08-26 19:13:37 -0500

    added port 3000 or heroku port logic

--- server/public/app.js
+++ server/public/app.js
@@ -9,8 +9,7 @@ app.use('/employees', employees);
 app.use('/scripts', express.static(path.join(__dirname, './assets/scripts')));
 app.use('/styles', express.static(path.join(__dirname, './assets/styles')));
 
-var server = app.listen(3000,function(){
+var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
-    console.log('App listening on ', port);
-
+    console.log('listening for traffic on port ', port);
 });
